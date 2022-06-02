7c2f45e675d8186672b57289819df3d62d70406f
“Will <noptov52@yahoo.com>
2022-06-01 20:09:34 -0400

    Streamlit app functioning locally

--- pokeid-app.py
+++ pokeid-app.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import Resizing
+# from PIL import Image
 
 size = (128, 128)
 
@@ -16,29 +18,32 @@ model_dict = {}
 for name in type_names:
     model_dict[name] = tf.keras.models.load_model(f'Models/{name}_model')
 
-pokemon_dict= pd.read_csv('data/pokemon_class_dict.csv', header=None, index_col=0, squeeze=True).to_dict()
+pokemon_dict= pd.read_csv('Data/pokemon_class_dict.csv', header=None, index_col=0, squeeze=True).to_dict()
 
 st.title('Welcome to the Pokemon ID Project') 
 
 # Prompt user to upload an image and convert that file into an array/batch the models can use to predict
-user_pic = st.file_uploader(label='Please upload your picture to identify what pokemon it is.',
-                    type=['png', 'jpg']
-                    )
+user_pic_file = st.file_uploader(label='Please upload your picture to identify what pokemon it is.',
+                    type=['png', 'jpg', 'jpeg'])
 
-user_array = tf.keras.preprocessing.image.smart_resize(
-    user_pic, size, interpolation='bilinear'
-)
-user_batch = np.expand_dims(test_array2, axis=0)
+if user_pic_file is not None:
+    st.subheader("Your image")
+    user_img = image.load_img(user_pic_file, target_size=(128, 128))
+    st.image(user_img)
+    user_array = image.img_to_array(user_img)
+   
 
-# Use converted image to predict type of pokemon
-user_pred = type_model.predict(user_batch)
-user_type = type_names[np.argmax(user_pred)]
-st.write(f'The type of Pokemon is {user_type}')
+    user_batch = np.expand_dims(user_array, axis=0)
 
-# Based on type, use appropriate model to ID individual pokemon
-ind_model = model_dict[user_type]
-ind_pred = ind_model.predict(user_batch)
-ind_list = pokemon_dict[user_type].split(',')
-ind_pokemon = ind_list[np.argmax(ind_pred)]
+    # Use converted image to predict type of pokemon
+    user_pred = type_model.predict(user_batch)
+    user_type = type_names[np.argmax(user_pred)]
+    st.write(f'The type of Pokemon is {user_type}')
 
-st.write(f'Your Pokemon is {ind_pokemon}!')
+    # Based on type, use appropriate model to ID individual pokemon
+    ind_model = model_dict[user_type]
+    ind_pred = ind_model.predict(user_batch)
+    ind_list = pokemon_dict[user_type].split(',')
+    ind_pokemon = ind_list[np.argmax(ind_pred)]
+
+    st.write(f'Your Pokemon is {ind_pokemon}!')
